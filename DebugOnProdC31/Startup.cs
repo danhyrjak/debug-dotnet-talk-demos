a2b7e45f2f9b086041c72921e95c932b3643c2dc
Dan <dan_hyrjak@hotmail.co.uk>
2020-01-29 23:37:35 +0000

    added application insights via connected services option to dotnet core project

--- DebugOnProdC31/Startup.cs
+++ DebugOnProdC31/Startup.cs
@@ -30,6 +30,7 @@ namespace DebugOnProdC31
                 options.UseSqlServer(Configuration.GetConnectionString("RegistrationSQLConnection"));
             });
             services.AddControllersWithViews();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
