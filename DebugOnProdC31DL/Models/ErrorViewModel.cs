f38250f0674faf0eb71e6fce2c14b563a722922b
Dan Hyrjak <dan.hyrjak@prospectsoft.com>
2020-01-29 13:18:11 +0000

    initial commit with four MVC starter projects setup as: DebugOnProdF452 - .NET Framework 4.5.2 (no container) DebugOnProdC31 - .NET core 3.1 (no container) DebugOnProdC31DL - .NET core 3.1 (linux docker container) DebugOnProdC31DW - .NET core 3.1 (windows docker container)

---
+++ DebugOnProdC31DL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DebugOnProdC31DL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
